136a4eafc98f38957311547d6cb537354c8b8d17
YunJaeHoon <sleepzzz214@naver.com>
2025-03-20 15:07:29 +0900

    feat: 기본 프레임 및 컨텐츠 펜 생성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import frame.MainFrame;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MainFrame mainFrame = new MainFrame();
     }
 }
---
+++ src/frame/MainFrame.java
@@ -0,0 +1,22 @@
+package frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame
+{
+    public MainFrame()
+    {
+        setTitle("그림을 그려보아요~");    // 프레임 제목
+        setSize(500, 300);              // 프레임 크기
+
+
+        // 종료 버튼 동작 설정
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();   // 프레임에서 컨텐츠 펜 받아오기
+        contentPane.setLayout(null);                // AbsoluteLayout 사용
+
+        setVisible(true);   // 화면에 프레임 출력
+    }
+}
